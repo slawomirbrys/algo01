0b42a4ea330d307fbf43f78bc2cfeb4cc70e6791
Sławomir Bryś <slawomir.brys@gmail.com>
2021-02-13 22:58:33 +0100

    added time elapsed to log

--- helpers.js
+++ helpers.js
@@ -9,8 +9,17 @@ function shouldDo(p) {
 }
 
 function log(object) {
+  if(typeof(object) === 'function') {
+    let start = new Date();
+    let result = object();
+    let end = new Date();
+    console.log(`[${end-start}ms] `, util.inspect(result, true, 100, true));
+    return;
+  }
+
   //console.log(JSON.stringify(object, null, 2));
   console.log(util.inspect(object, true, 100, true));
 }
+
 
 export { getValue, shouldDo, log }
