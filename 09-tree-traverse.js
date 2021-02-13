8ed686832244cf5ed4fc45505305568a92e4f69e
Sławomir Bryś <slawomir.brys@gmail.com>
2021-02-13 21:29:38 +0100

    09 added

--- 09-tree-traverse.js
+++ 09-tree-traverse.js
@@ -1,6 +1,18 @@
 import { TreeNode, createTreeNode } from './tree-binary.js';
 import { log } from './helpers.js';
 
+function traverse(node) {
+  if(node === null) return;
+
+  console.log(node.value);
+  traverse(node.left);
+  traverse(node.right);
+}
+
 let head = createTreeNode(3);
 log('tree');
 log(head);
+
+log('traverse');
+traverse(head);
+
