7be76963f977737db22837150fe1ba3ccae1222f
Sławomir Bryś <slawomir.brys@gmail.com>
2021-02-13 22:58:53 +0100

    added can-sum

---
+++ 11-can-sum.js
@@ -0,0 +1,35 @@
+
+import { log } from './helpers.js';
+
+/**
+ * Given array of numbers of N, return true/false
+ * if can have numbers from arrays that sums to the 
+ * given one SUM.
+ * 
+ */
+
+function canSum(sum, array) {
+  if(sum === 0) return true;
+  if(sum <= 0) return false;
+  if(array === null) return false;
+
+  let result = false;
+  for(let i=0; i<array.length; i++) {
+    result = canSum(sum - array[i], array);
+    if(result === true)
+      return true;
+  }
+
+  return false;
+}
+
+
+log(() => canSum(0, null));
+log(() => canSum(7, null));
+log(() => canSum(7, []));
+log(() => canSum(7, [1]));
+log(() => canSum(7, [7]));
+log(() => canSum(7, [3, 4, 5]));
+log(() => canSum(7, [5, 3, 4, 7, 9]));
+log(() => canSum(7, [4, 10]));
+log(() => canSum(300, [7, 14]));
---
+++ 12-can-sum-memoized.js
@@ -0,0 +1,37 @@
+
+import { log } from './helpers.js';
+
+/**
+ * Given array of numbers of N, return true/false
+ * if can have numbers from arrays that sums to the 
+ * given one SUM.
+ * 
+ */
+
+function canSum(sum, array, memo = {}) {
+  if(sum in memo) return memo[sum];
+  if(sum === 0) return true;
+  if(sum <= 0) return false;
+  if(array === null) return false;
+
+  let result = false;
+  for(let i=0; i<array.length; i++) {
+    result = canSum(sum - array[i], array, memo);
+    memo[sum] = result;
+    if(result === true)
+      return true;
+  }
+
+  return false;
+}
+
+
+log(() => canSum(0, null));
+log(() => canSum(7, null));
+log(() => canSum(7, []));
+log(() => canSum(7, [1]));
+log(() => canSum(7, [7]));
+log(() => canSum(7, [3, 4, 5]));
+log(() => canSum(7, [5, 3, 4, 7, 9]));
+log(() => canSum(7, [4, 10]));
+log(() => canSum(300, [7, 14]));
